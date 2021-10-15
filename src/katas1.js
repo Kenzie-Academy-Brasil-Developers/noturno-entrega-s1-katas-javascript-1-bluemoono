b23ffc1d85bafc4ee29f9490359fc4e85d8dda92
Bluemoono <mikka_paulina@live.com>
2021-10-15 20:07:27 -0300

    Resolução de atividades do Kata-01

--- src/katas1.js
+++ src/katas1.js
@@ -1,30 +1,38 @@
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+    /* Your code goes below
+    Write a for or a while loop
+    return the result*/
+    for(let i=1;i<=20; i++){
+      console.log(i);
+    }
 }
+//oneThroughTwenty();
 
 //call function oneThroughTwenty
 
 function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+    /* Your code goes below
+    Write a for or a while loop
+    return the result */
+    for(let i=2;i<=20; i+=2){
+      console.log(i);
+    }
 }
+//evensToTwenty();
 
 //call function evensToTwenty
 
 function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+    /* Your code goes below
+    Write a for or a while loop
+    return the result */
+  let i=1;
+  while(i<=20){
+    console.log(i);
+    i+=2;
+  }
 }
+//oddsToTwenty();
 
 //call function oddsToTwenty
 
@@ -33,18 +41,29 @@ function multiplesOfFive() {
   /* Your code goes below
   Write a for or a while loop
   return the result */
+  let i=5;
+  while(i<=100){
+    console.log(i);
+    i+=5;
+  }
 
 }
-
+//multiplesOfFive();
 //call function multiplesOfFive
 
 function squareNumbers() {
-    
- /* Your code goes below
+  /* Your code goes below
   Write a for or a while loop
   return the result */
-    
+  
+  for(let i=1; i<=100; i++){
+    let perfeito = i*i;
+    if(perfeito<=100){
+      console.log(perfeito)
+    }
+  }
 }
+//squareNumbers()
 
 //call function squareNumbers
 
@@ -53,17 +72,24 @@ function countingBackwards() {
   /* Your code goes below
   Write a for or a while loop
   return the result */
+  for(let i=20; i>=1;i--){
+    console.log(i);
+  }
 }
+//countingBackwards();
 
 //call function countingBackwards
 
 function evenNumbersBackwards() {
-    
  /* Your code goes below
   Write a for or a while loop
   return the result */
+  for (let index = 20; index >= 1; index-=2) {
+    console.log(index)
+    
+  }
 }
-
+//evenNumbersBackwards();
 //call function evenNumbersBackwards
 
 function oddNumbersBackwards() {
@@ -71,8 +97,14 @@ function oddNumbersBackwards() {
  /* Your code goes below
   Write a for or a while loop
   return the result */
+  let i = 20;
+  i--
+  while(i>=1){
+    console.log(i);
+    i-=2;
+  }
 }
-
+//oddNumbersBackwards();
 //call function oddNumbersBackwards
 
 function multiplesOfFiveBackwards() {
@@ -80,8 +112,11 @@ function multiplesOfFiveBackwards() {
  /* Your code goes below
   Write a for or a while loop
   return the result */
+  for(let i=100;i>=1; i-=5){
+    console.log(i)
+  }
 }
-
+//multiplesOfFiveBackwards();
 //call function multiplesOfFiveBackwards
 
 function squareNumbersBackwards() {
@@ -89,6 +124,13 @@ function squareNumbersBackwards() {
    /* Your code goes below
   Write a for or a while loop
   return the result */
-}
 
+  for(let i=100; i>=1; i--){
+    let perfeito = i*i;
+    if(perfeito<=100){
+      console.log(perfeito)
+    }
+  }
+}
+squareNumbersBackwards()
 //call function squareNumbersBackwards
